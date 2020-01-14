d75871d5139f6cee987f7f2e823cb92054444127
Adonis Stavridis <adonis.s@windowslive.com>
2020-01-14 05:02:23 +0100

    Added more characters

--- extension.js
+++ extension.js
@@ -29,11 +29,20 @@ function activate(context) {
 	vscode.languages.registerHoverProvider('*', {
 		provideHover(document, position) {
 			const wordRange = document.getWordRangeAtPosition(position);
-			const word = document.getText(wordRange);
+			const word = document.getText(wordRange).toLowerCase();
 
 			const map = {
-				doge: '![Doge](https://raw.githubusercontent.com/Adonis-Stavridis/Doge-Extension/master/imgs/doge.png "Doge")',
+				big_bro: '![Big Bro](https://raw.githubusercontent.com/Adonis-Stavridis/Doge-Extension/master/imgs/big_bro.png "Big Bro")',
 				cheems: '![Cheems](https://raw.githubusercontent.com/Adonis-Stavridis/Doge-Extension/master/imgs/cheems.png "Cheems")',
+				doge: '![Doge](https://raw.githubusercontent.com/Adonis-Stavridis/Doge-Extension/master/imgs/doge.png "Doge")',
+				isabelle: '![Isabelle](https://raw.githubusercontent.com/Adonis-Stavridis/Doge-Extension/master/imgs/isabelle.png "Isabelle")',
+				karen: '![Karen](https://raw.githubusercontent.com/Adonis-Stavridis/Doge-Extension/master/imgs/karen.png "Karen")',
+				lil_bro: '![Lil Bro](https://raw.githubusercontent.com/Adonis-Stavridis/Doge-Extension/master/imgs/lil_bro.png "Lil Bro")',
+				milton: '![Milton](https://raw.githubusercontent.com/Adonis-Stavridis/Doge-Extension/master/imgs/milton.png "Milton")',
+				petyr: '![Petyr](https://raw.githubusercontent.com/Adonis-Stavridis/Doge-Extension/master/imgs/petyr.png "Petyr")',
+				quieres: '![Quieres](https://raw.githubusercontent.com/Adonis-Stavridis/Doge-Extension/master/imgs/quieres.png "Quieres")',
+				swagcat: '![Swagcat](https://raw.githubusercontent.com/Adonis-Stavridis/Doge-Extension/master/imgs/swagcat.png "Swagcat")',
+				walter: '![Walter](https://raw.githubusercontent.com/Adonis-Stavridis/Doge-Extension/master/imgs/walter.png "Walter")'
 			};
 
 			var popup = new vscode.Hover(map[word]);
