3a945f048142da13372eda8844a804d65804d920
Adonis Stavridis <adonis.stavridis@gmail.com>
2020-09-27 23:58:49 +0200

    add image to canvas

--- template/app/doge.js
+++ template/app/doge.js
@@ -1,16 +1,33 @@
-// var imgPath = "../img/";
-// var images = [];
+$(document).ready(function () {
 
-// $(document).ready(function () {
-//   readImages();
-//   // loadImages();
-// });
+  $(".imagesContainer img").click(function () {
+    const imgSrc = $(this).attr('src');
+    addToCanvas(imgSrc);
+  });
 
-// $(function readImages() {
-//   // images = fs.readdirSync(imgPath);
-//   console.log(images);
-// });
+  $(".renderButton").click(function () {
+    const canvasElement = document.getElementById('memeCanvas');
+    const MIME_TYPE = "image/png";
+    const imgURL = canvasElement.toDataURL(MIME_TYPE);
 
-// $(function loadImages() {
+    const dlLink = document.createElement('a');
+    dlLink.download = "meme.png";
+    dlLink.href = imgURL;
+    dlLink.dataset.downloadurl = [MIME_TYPE, dlLink.download, dlLink.href].join(':');
 
-// });
+    document.body.appendChild(dlLink);
+    dlLink.click();
+    document.body.removeChild(dlLink);
+  });
+
+  function addToCanvas(imgSrc) {
+    const canvas = document.getElementById('memeCanvas');
+    const context = canvas.getContext('2d');
+
+    newImage = new Image();
+    newImage.src = imgSrc;
+    newImage.onload = function () {
+      context.drawImage(newImage, 0, 0);
+    };
+  }
+});
