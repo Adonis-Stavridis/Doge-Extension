505a182bddd6e14b8286d1ba3ae883fe7c3d7d79
Adonis Stavridis <adonis.stavridis@gmail.com>
2020-09-29 23:10:54 +0200

    imageEdit update

--- template/app/doge.js
+++ template/app/doge.js
@@ -3,18 +3,21 @@ $(document).ready(function () {
   const canvas = document.getElementById("memeCanvas");
   const context = canvas.getContext('2d');
 
-  context.canvas.width = canvas.width;
-  context.canvas.height = canvas.height;
+  context.canvas.width = canvas.clientWidth;
+  context.canvas.height = canvas.clientHeight;
+
+  var imagesSet = new Set();
 
   $(window).resize(function () {
     const canvasElement = document.getElementById("memeCanvas");
-    context.canvas.width = canvasElement.width;
-    context.canvas.height = canvasElement.height;
+    context.canvas.width = canvasElement.clientWidth;
+    context.canvas.height = canvasElement.clientHeight;
   });
 
   $(".imagesContainer img").click(function () {
     const imgSrc = $(this).attr('src');
-    addToCanvas(imgSrc);
+    const imgAlt = $(this).attr('alt');
+    addImage(imgSrc, imgAlt);
   });
 
   $(".renderButton").click(function () {
@@ -22,7 +25,7 @@ $(document).ready(function () {
     const MIME_TYPE = "image/png";
     const imgURL = canvasElement.toDataURL(MIME_TYPE);
 
-    const dlLink = document.createElement('a');
+    const dlLink = document.createElement("a");
     dlLink.download = "meme.png";
     dlLink.href = imgURL;
     dlLink.dataset.downloadurl = [MIME_TYPE, dlLink.download, dlLink.href].join(':');
@@ -32,16 +35,103 @@ $(document).ready(function () {
     document.body.removeChild(dlLink);
   });
 
-  function addToCanvas(imgSrc) {
-    newImage = new Image();
+  function addImage(imgSrc, imgAlt) {
+    var newImage = new Image();
     newImage.src = imgSrc;
     newImage.onload = function () {
       context.drawImage(newImage, 0, 0);
     };
+
+    var newImgAlt = imgAlt;
+    while (imagesSet.has(newImgAlt)) {
+      newImgAlt = newImgAlt + "-copy";
+    }
+
+    newImage.alt = newImgAlt;
+
+    imagesSet.add(newImgAlt);
+    addImageEdit(imgSrc, newImgAlt);
   }
 
-  // TODO : addEvent procedurally
-  $(".imagesEdit i").click(function () {
-    $(this).parent().remove();
-  });
+  function addImageEdit(imgSrc, imgAlt) {
+    var newImageEdit = document.createElement("div");
+    newImageEdit.classList.add("imageEdit");
+
+    var asset = new Image();
+    asset.src = imgSrc;
+    asset.alt = imgAlt;
+    newImageEdit.appendChild(asset);
+
+    asset = document.createElement("span");
+    asset.textContent = "Pos:";
+    newImageEdit.appendChild(asset);
+
+    asset = document.createElement("input");
+    asset.type = "number";
+    asset.placeholder = "x";
+    asset.required = true;
+    newImageEdit.appendChild(asset);
+
+    asset = document.createElement("input");
+    asset.type = "number";
+    asset.placeholder = "y";
+    asset.required = true;
+    newImageEdit.appendChild(asset);
+
+    asset = document.createElement("span");
+    asset.textContent = "Size:";
+    newImageEdit.appendChild(asset);
+
+    asset = document.createElement("input");
+    asset.type = "number";
+    asset.placeholder = "x";
+    asset.required = true;
+    newImageEdit.appendChild(asset);
+
+    asset = document.createElement("input");
+    asset.type = "number";
+    asset.placeholder = "y";
+    asset.required = true;
+    newImageEdit.appendChild(asset);
+
+    asset = document.createElement("span");
+    asset.textContent = "Rot:";
+    newImageEdit.appendChild(asset);
+
+    asset = document.createElement("input");
+    asset.type = "number";
+    asset.placeholder = "deg";
+    asset.required = true;
+    newImageEdit.appendChild(asset);
+
+    asset = document.createElement("button");
+    asset.classList.add("flipButton");
+    asset.textContent = "Flip";
+    newImageEdit.appendChild(asset);
+
+    asset = document.createElement("span");
+    asset.textContent = "Layer:";
+    newImageEdit.appendChild(asset);
+
+    asset = document.createElement("input");
+    asset.type = "number";
+    asset.placeholder = "num";
+    asset.required = true;
+    newImageEdit.appendChild(asset);
+
+    asset = document.createElement("i");
+    asset.classList.add("fas");
+    asset.classList.add("fa-trash");
+    asset.addEventListener("click", function () {
+      removeImage(imgAlt);
+    });
+    newImageEdit.appendChild(asset);
+
+    const imagesEditContainter = document.getElementById("imagesEditContainer");
+    imagesEditContainter.appendChild(newImageEdit);
+  }
+
+  function removeImage(imgAlt) {
+    imagesSet.delete(imgAlt);
+  }
 });
