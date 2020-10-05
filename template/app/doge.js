200bf26ed1de81d2db40a0bbedd56ad3065b8da8
Adonis Stavridis <adonis.stavridis@gmail.com>
2020-10-06 00:42:22 +0200

    img selection

--- template/app/doge.js
+++ template/app/doge.js
@@ -28,6 +28,9 @@ $(document).ready(function () {
   };
 
   var memeDiv = $("#memeDiv");
+  var editContainer = $("#editContainer");
+
+  var currentSelection = null;
 
   // ===========================================================================
   // ASSET HANDLING
@@ -41,8 +44,6 @@ $(document).ready(function () {
     imgObj.id = assetName;
     imgObj.alt = imgName;
     imgObj.src = imgSrc;
-
-    console.log(memeDiv.width() + "/" + memeDiv.height());
 
     const posObject = {
       x: Math.trunc((memeDiv.height() / 2) - (imgObj.height / 2)),
@@ -66,33 +67,159 @@ $(document).ready(function () {
     };
 
     addImgElements(assetName);
-    console.log(assets);
   }
 
   function addImgElements(assetName) {
     var imgElement = assets[assetName].image;
 
+    imgElement.id = assetName;
     imgElement.style.top = assets[assetName].pos.x + "px";
     imgElement.style.left = assets[assetName].pos.y + "px";
     imgElement.style.width = assets[assetName].size.x + "px";
     imgElement.style.height = assets[assetName].size.y + "px";
 
-    // imgElement.css({
-    //   top: assets[assetName].pos.x,
-    //   left: assets[assetName].pos.y,
-    //   // width: assets[assetName].size.x,
-    //   // height: assets[assetName].size.y
-    // });
+    imgElement.addEventListener("click", function () {
+      if (currentSelection) {
+        unsetSelection(currentSelection);
+      }
+      setSelection(assetName);
+    });
+
+    var newImageEdit = document.createElement("div");
+    newImageEdit.id = assetName;
+    newImageEdit.classList.add("imageEdit");
+    newImageEdit.addEventListener("click", function () {
+      if (currentSelection) {
+        unsetSelection(currentSelection);
+      }
+      setSelection(assetName);
+    });
+
+
+    var imageCopy = new Image();
+    imageCopy.alt = imgElement.alt;
+    imageCopy.src = imgElement.src;
+    newImageEdit.appendChild(imageCopy);
+
+    editElement = document.createElement("span");
+    editElement.textContent = "Pos:";
+    newImageEdit.appendChild(editElement);
+
+    editElement = document.createElement("input");
+    editElement.type = "number";
+    editElement.placeholder = "x";
+    editElement.value = assets[assetName].pos.x;
+    editElement.required = true;
+    editElement.addEventListener("input", function () {
+      inputEdit(assetName, "pos", "x", this.value);
+    });
+    newImageEdit.appendChild(editElement);
+
+    editElement = document.createElement("input");
+    editElement.type = "number";
+    editElement.placeholder = "y";
+    editElement.value = assets[assetName].pos.y;
+    editElement.required = true;
+    editElement.addEventListener("input", function () {
+      inputEdit(assetName, "pos", "y", this.value);
+    });
+    newImageEdit.appendChild(editElement);
+
+    editElement = document.createElement("span");
+    editElement.textContent = "Size:";
+    newImageEdit.appendChild(editElement);
+
+    editElement = document.createElement("input");
+    editElement.type = "number";
+    editElement.placeholder = "x";
+    editElement.value = assets[assetName].size.x;
+    editElement.required = true;
+    editElement.addEventListener("input", function () {
+      inputEdit(assetName, "size", "x", this.value);
+    });
+    newImageEdit.appendChild(editElement);
+
+    editElement = document.createElement("input");
+    editElement.type = "number";
+    editElement.placeholder = "y";
+    editElement.value = assets[assetName].size.y;
+    editElement.required = true;
+    editElement.addEventListener("input", function () {
+      inputEdit(assetName, "size", "y", this.value);
+    });
+    newImageEdit.appendChild(editElement);
+
+    editElement = document.createElement("span");
+    editElement.textContent = "Rot:";
+    newImageEdit.appendChild(editElement);
+
+    editElement = document.createElement("input");
+    editElement.type = "number";
+    editElement.placeholder = "deg";
+    editElement.value = assets[assetName].rot.deg;
+    editElement.required = true;
+    editElement.addEventListener("input", function () {
+      inputEdit(assetName, "rot", "deg", this.value);
+    });
+    newImageEdit.appendChild(editElement);
+
+    editElement = document.createElement("button");
+    editElement.classList.add("flipButton");
+    editElement.textContent = "Flip";
+    newImageEdit.appendChild(editElement);
+
+    editElement = document.createElement("span");
+    editElement.textContent = "Layer:";
+    newImageEdit.appendChild(editElement);
+
+    editElement = document.createElement("input");
+    editElement.type = "number";
+    editElement.placeholder = "num";
+    editElement.value = assets[assetName].layer;
+    editElement.required = true;
+    newImageEdit.appendChild(editElement);
+
+    editElement = document.createElement("i");
+    editElement.classList.add("fas");
+    editElement.classList.add("fa-trash");
+    editElement.addEventListener("click", function () {
+      removeImage(assetName);
+    });
+    newImageEdit.appendChild(editElement);
 
     memeDiv.append(imgElement);
+    editContainer.append(newImageEdit);
+  }
+
+  function inputEdit(assetName, object, key, value) {
+    if (!isNaN(value)) {
+      assets[assetName][object][key] = parseInt(value);
+    }
+  }
+
+  function removeImage(assetName) {
+    if (currentSelection === assetName) {
+      unsetSelection(currentSelection);
+      currentSelection = null;
+    }
+    delete assets[assetName];
+    $("#memeDiv img").remove("#" + assetName);
+    $("#editContainer div").remove("#" + assetName);
+  }
+
+  function setSelection(assetName) {
+    currentSelection = assetName;
+    $(".memeDiv img#" + assetName).addClass("selected");
+    $(".editContainer div#" + assetName).addClass("selected");
+  }
+
+  function unsetSelection(assetName) {
+    $(".memeDiv img#" + assetName).removeClass("selected");
+    $(".editContainer div#" + assetName).removeClass("selected");
   }
 
   // ADD TEXT ASSET
   function textAsset(textContent) {}
-
-  function updateMemeDiv() {
-    console.log(assets);
-  }
 
   // ===========================================================================
   // EVENTS
