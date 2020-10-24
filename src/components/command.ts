74b0b11dfcf717a08f8f66e360b96c1eea025ec3
Adonis Stavridis <adonis.stavridis@gmail.com>
2020-10-24 22:46:48 +0200

    remove statusBarItem

--- src/components/command.ts
+++ src/components/command.ts
@@ -1,15 +1,12 @@
 import * as vscode from "vscode";
 
-import { dogeUpdateStatusBarItem } from "./statusBarItem";
 import { dogeMeme } from "./meme";
 
 export let dogeCommandID = "doge.createMeme";
 
 export async function dogeCommand(context: vscode.ExtensionContext) {
   let command = vscode.commands.registerCommand(dogeCommandID, async function () {
-    dogeUpdateStatusBarItem(true);
     await dogeMeme();
-    dogeUpdateStatusBarItem(false);
   });
 
   context.subscriptions.push(command);
--- src/extension.ts
+++ src/extension.ts
@@ -2,14 +2,11 @@ import * as vscode from "vscode";
 
 import { dogeCommand } from "./components/command";
 import { dogeHoverProvider } from "./components/hover";
-import { dogeCreateStatusBarItem } from "./components/statusBarItem";
 
 export function activate(context: vscode.ExtensionContext) {
   dogeCommand(context);
 
   dogeHoverProvider();
-
-  dogeCreateStatusBarItem(context);
 }
 
 export function deactivate() { }
