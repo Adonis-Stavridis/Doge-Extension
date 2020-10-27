047117df40cc6f7bfc6a4222318d7b37fad26acf
Adonis Stavridis <adonis.stavridis@gmail.com>
2020-10-27 14:37:35 +0100

    draggable  asset edits + small bug

--- template/app/doge.js
+++ template/app/doge.js
@@ -62,27 +62,25 @@ function addImgElements(assetName) {
   imgElement.style.width = assets[assetName].size.x + "px";
   imgElement.style.height = assets[assetName].size.y + "px";
   imgElement.draggable = false;
-  imgElement.addEventListener("click", function () {
-    if (currentSelection) {
-      unsetSelection(currentSelection);
-    }
-    setSelection(assetName);
-  });
 
   var newImageEdit = document.createElement("div");
   newImageEdit.id = assetName;
   newImageEdit.classList.add("imageEdit");
-  newImageEdit.addEventListener("click", function () {
+  newImageEdit.classList.add("draggable");
+  newImageEdit.addEventListener("click", function (e) {
     if (currentSelection) {
       unsetSelection(currentSelection);
     }
     setSelection(assetName);
+    e.stopPropagation();
   });
+  newImageEdit.addEventListener('mousedown', mouseDownHandler);
 
   var imageCopy = new Image();
   imageCopy.classList.add("icon");
   imageCopy.alt = imgElement.alt;
   imageCopy.src = imgElement.src;
+  imageCopy.draggable = false;
   newImageEdit.appendChild(imageCopy);
 
   editElement = document.createElement("span");
@@ -407,3 +405,138 @@ function drag(e) {
     updateAsset(currentSelection, "pos", "y", currentY, true);
   }
 }
+
+// ===========================================================================
+// DRAG ASSET EDITS
+// ===========================================================================
+
+let draggingEle;
+let elewidth;
+let placeholder;
+let isDraggingStarted = false;
+let mousehold = false;
+
+// The current position of mouse relative to the dragging element
+let x = 0;
+let y = 0;
+
+// Swap two nodes
+const swap = function(nodeA, nodeB) {
+  const parentA = nodeA.parentNode;
+  const siblingA = nodeA.nextSibling === nodeB ? nodeA : nodeA.nextSibling;
+
+  // Move `nodeA` to before the `nodeB`
+  nodeB.parentNode.insertBefore(nodeA, nodeB);
+
+  // Move `nodeB` to before the sibling of `nodeA`
+  parentA.insertBefore(nodeB, siblingA);
+};
+
+// Check if `nodeA` is above `nodeB`
+const isAbove = function(nodeA, nodeB) {
+  // Get the bounding rectangle of nodes
+  const rectA = nodeA.getBoundingClientRect();
+  const rectB = nodeB.getBoundingClientRect();
+
+  return (rectA.top + rectA.height / 2 < rectB.top + rectB.height / 2);
+};
+
+const mouseDownHandler = function (e) {
+  if (!e.target.classList.contains("imageEdit")) {
+    return;
+  }
+    
+  draggingEle = e.target;
+
+  // Calculate the mouse position
+  const rect = draggingEle.getBoundingClientRect();
+  x = e.pageX - rect.left;
+  y = e.pageY - rect.top;
+
+  // Attach the listeners to `document`
+  document.addEventListener('mousemove', mouseMoveHandler);
+  document.addEventListener('mouseup', mouseUpHandler);
+
+  mousehold = true;
+};
+
+const mouseMoveHandler = function (e) {
+  if (!mousehold) {
+    return;
+  }
+
+  const draggingRect = draggingEle.getBoundingClientRect();
+
+  if (!isDraggingStarted) {
+    isDraggingStarted = true;
+    
+    // Let the placeholder take the height of dragging element
+    // So the next element won't move up
+    placeholder = document.createElement('div');
+    placeholder.classList.add('placeholder');
+    draggingEle.parentNode.insertBefore(placeholder, draggingEle.nextSibling);
+    placeholder.style.height = draggingRect.height + "px";
+    placeholder.style.minHeight = draggingRect.height + "px";
+    elewidth = draggingEle.clientWidth;
+  }
+
+  // Set position for dragging element
+  draggingEle.style.position = 'absolute';
+  draggingEle.style.top = `${e.pageY - y}px`; 
+  draggingEle.style.left = `${e.pageX - x}px`;
+  draggingEle.style.width = elewidth + "px";
+
+  // The current order
+  // prevEle
+  // draggingEle
+  // placeholder
+  // nextEle
+  const prevEle = draggingEle.previousElementSibling;
+  const nextEle = placeholder.nextElementSibling;
+  
+  // The dragging element is above the previous element
+  // User moves the dragging element to the top
+  if (prevEle && isAbove(draggingEle, prevEle)) {
+    // The current order    -> The new order
+    // prevEle              -> placeholder
+    // draggingEle          -> draggingEle
+    // placeholder          -> prevEle
+    swap(placeholder, draggingEle);
+    swap(placeholder, prevEle);
+    return;
+  }
+
+  // The dragging element is below the next element
+  // User moves the dragging element to the bottom
+  if (nextEle && isAbove(nextEle, draggingEle)) {
+    // The current order    -> The new order
+    // draggingEle          -> nextEle
+    // placeholder          -> placeholder
+    // nextEle              -> draggingEle
+    swap(nextEle, placeholder);
+    swap(nextEle, draggingEle);
+  }
+};
+
+const mouseUpHandler = function () {
+  mousehold = false;
+
+  // Remove the placeholder
+  if (isDraggingStarted) {
+    placeholder && placeholder.parentNode.removeChild(placeholder);draggingEle.style.removeProperty('top');
+    
+    draggingEle.style.removeProperty('left');
+    draggingEle.style.removeProperty('position');
+
+    isDraggingStarted = false;
+  }
+
+  x = null;
+  y = null;
+  elewidth = 0;
+  draggingEle = null;
+
+  // Remove the handlers of `mousemove` and `mouseup`
+  document.removeEventListener('mousemove', mouseMoveHandler);
+  document.removeEventListener('mouseup', mouseUpHandler);
+};
